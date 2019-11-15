db5500900fce04ae403e322abd62af0f174c3f2b
Aman Aryan <aman.a@scroll.in>
2019-11-15 14:10:07 +0530

    appearance and style changes

--- components/Home.vue
+++ components/Home.vue
@@ -1,110 +1,96 @@
 <template>
-  <!--Container-->
   <div class="container w-full mx-auto pt-20">
-    <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-300 leading-normal">
-      <!--Console Content-->
+    <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
       <div class="flex flex-wrap">
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <!--Metric Card-->
-          <div class="bg-gray-600 border border-gray-200 rounded shadow p-2">
+          <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
             <div class="flex flex-row items-center">
               <div class="flex-1 text-right md:text-center">
                 <h5
-                  class="font-bold uppercase text-gray-300"
+                  class="font-bold uppercase text-gray-400"
                 >Total Comments
                 </h5>
-                <h3 class="font-bold text-3xl text-gray-300">
+                <h3 class="font-bold text-3xl text-gray-600">
                   3249
                 </h3>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <!--Metric Card-->
-          <div class="bg-gray-600 border border-gray-200 rounded shadow p-2">
+          <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
             <div class="flex flex-row items-center">
               <div class="flex-1 text-right md:text-center">
                 <h5
-                  class="font-bold uppercase text-gray-300"
+                  class="font-bold uppercase text-gray-400"
                 >Total Accepted Comments
                 </h5>
-                <h3 class="font-bold text-3xl text-gray-300">
+                <h3 class="font-bold text-3xl text-gray-600">
                   332
                 </h3>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <!--Metric Card-->
-          <div class="bg-gray-600 border border-gray-200 rounded shadow p-2">
+          <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
             <div class="flex flex-row items-center">
               <div class="flex-1 text-right md:text-center">
                 <h5
-                  class="font-bold uppercase text-gray-300"
+                  class="font-bold uppercase text-gray-400"
                 >Total Rejected Comments
                 </h5>
-                <h3 class="font-bold text-3xl text-gray-300">
+                <h3 class="font-bold text-3xl text-gray-600">
                   3249
                 </h3>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <!--Metric Card-->
-          <div class="bg-gray-600 border border-gray-200 rounded shadow p-2">
+          <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
             <div class="flex flex-row items-center">
               <div class="flex-1 text-right md:text-center">
                 <h5
-                  class="font-bold uppercase text-gray-300"
+                  class="font-bold uppercase text-gray-400"
                 >Active Articles
                 </h5>
-                <h3 class="font-bold text-3xl text-gray-300">
+                <h3 class="font-bold text-3xl text-gray-600">
                   3249
                 </h3>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <!--Metric Card-->
-          <div class="bg-gray-600 border border-gray-200 rounded shadow p-2">
+          <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
             <div class="flex flex-row items-center">
               <div class="flex-1 text-right md:text-center">
                 <h5
-                  class="font-bold uppercase text-gray-300"
+                  class="font-bold uppercase text-gray-400"
                 > Total Articles
                 </h5>
-                <h3 class="font-bold text-3xl text-gray-300">
+                <h3 class="font-bold text-3xl text-gray-600">
                   3249
                 </h3>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <!--Metric Card-->
-          <div class="bg-gray-600 border border-gray-200 rounded shadow p-2">
+          <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
             <div class="flex flex-row items-center">
               <div class="flex-1 text-right md:text-center">
                 <h5
-                  class="font-bold uppercase text-gray-300"
+                  class="font-bold uppercase text-gray-400"
                 >Total Featured Comments
                 </h5>
-                <h3 class="font-bold text-3xl text-gray-300">
+                <h3 class="font-bold text-3xl text-gray-600">
                   3249
                 </h3>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="bg-gray-900">
-    <div class="container w-1/2 pl-2 md:pl-0 text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold items-center">Comments DashBoard</div>
+  <div class="bg-black-alt font-sans w-full min-h-screen m-0">
+    <div class="bg-gray-900 shadow">
+      <div class="container mx-auto px-4">
+        <div class="flex items-center justify-between py-4">
+          <div class="mx-auto uppercase text-gray-100 text-base xl:text-2xl no-underline hover:no-underline font-bold">Comments DashBoard</div>
+        </div>
+      </div>
+    </div>
     <div>
       <Home />
     </div>
@@ -18,31 +24,7 @@ export default {
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .bg-black-alt  {
+    background:#191919;
+  }
 </style>
