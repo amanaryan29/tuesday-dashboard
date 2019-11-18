7667743fc52517ea3f74250c4e8b5f6ae825f4fd
Aman Aryan <aman.a@scroll.in>
2019-11-18 15:24:43 +0530

    Tabs added to the navbar

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,15 @@
     <div class="bg-gray-900 shadow">
       <div class="container mx-auto px-4">
         <div class="flex items-center justify-between py-4">
+          <nav class="inline-block flex flex-col sm:flex-row">
+            <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none text-blue-500 border-b-2 font-medium border-blue-500">
+              All Stat
+            </button><button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
+              Monthly
+            </button><button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
+              Weekly
+            </button>
+          </nav>
           <div class="mx-auto uppercase text-gray-100 text-base xl:text-2xl no-underline hover:no-underline font-bold">Comments DashBoard</div>
         </div>
       </div>
