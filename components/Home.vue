eed685bc579c3bc8732f8f247ff775cf1ed65ede
Aman Aryan <aman.a@scroll.in>
2019-12-12 16:53:28 +0530

    directory structure changed

--- components/Home.vue
+++ components/Home.vue
@@ -8,7 +8,7 @@
       </div>
     </div>
     <div class="w-full mx-auto container sm:w-1/2 p-3">
-      <LineChartFinal />
+      <LineChart />
       <div class="bg-gray-900 border border-gray-800 rounded shadow p-2 mb-3">
         <div class="flex flex-row items-center">
           <div class="flex-1 text-right md:text-center">
@@ -23,14 +23,14 @@
 <script>
 import CardStat from '@/components/CardStat'
 import TableStat from '@/components/TableStat'
-import LineChartFinal from '@/components/LineChartFinal'
+import LineChart from '@/components/LineChart/index'
 
 export default {
   name: 'Home',
   components: {
     CardStat,
     TableStat,
-    LineChartFinal
+    LineChart
   }
 }
 </script>
